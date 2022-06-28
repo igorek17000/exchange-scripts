67e09498c5cc6922b4280c70f7fcbf17213be11f
Rudi Starcevic <rudi.starcevic@pm.me>
2022-06-28 18:51:42 -0500

    Updates

--- test/position-size.test.ts
+++ test/position-size.test.ts
@@ -2,10 +2,10 @@ import { expect, test } from "vitest"
 import { main } from "../src/position-size"
 
 test("#positionSize()", () => {
-  const market = "POLK-USDT"
-  const entryPrice = 0.06329
-  const stopLossPrice = 0.05561
-  const targetPrice = 0.07737
+  const market = "EDG-USDT"
+  const entryPrice = 0.001833
+  const stopLossPrice = 0.001654
+  const targetPrice = 0.002085
   const res = main(market, entryPrice, stopLossPrice, targetPrice)
   expect(res).eq(2001)
 })
